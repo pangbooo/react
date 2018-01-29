96e992f0509e39cb806f683183678b42914694c5
pangbo <1838529223@qq.com>
2018-01-29 15:07:26 +0800

    37.

---
+++ react-redux-1/src/index.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Header from './Header'
+import Content from './Content'
+import registerServiceWorker from './registerServiceWorker';
+
+function createStore (reducer) {
+    let state = null;
+    const listeners = [];
+    const subscribe = (listener) => listeners.push(listener);
+    const getState = () => state; //获取state
+    const dispatch = (action) => {  //修改state
+        state = reducer(state, action);
+        listeners.forEach((listener) => listener());//每次dispatch 之后自动执行
+    };
+    dispatch({}) ;// 初始化 state
+    return { getState, dispatch, subscribe }
+}
+
+const themeReducer = (state, action) => {
+    if (!state) return {
+        themeColor: 'red'
+    };
+    switch (action.type) {
+        case 'CHANGE_COLOR':
+            return { ...state, themeColor: action.themeColor };
+        default:
+            return state
+    }
+};
+
+const store = createStore(themeReducer);
+
+class Index extends Component{
+    static childContextTypes={
+        store : PropTypes.object
+    };
+
+    getChildContext(){
+        return { store }
+    }
+    render(){
+        return(
+            <div>
+                <Header />
+                <Content />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Index />,
+    document.getElementById('root'));
+registerServiceWorker();
