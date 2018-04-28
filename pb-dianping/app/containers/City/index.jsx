77e72a4f1e776f989b923daba16513e6d509b478
pangbo <1838529223@qq.com>
2018-04-28 16:25:31 +0800

    城市页面

--- pb-dianping/app/containers/City/index.jsx
+++ pb-dianping/app/containers/City/index.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { hashHistory } from 'react-router'
+import * as userinfoActionsFormOtherFile from '../../actions/userinfo.js'
+import localStore from '../../util/localStore'
+import {CITYNAME} from '../../config/localStoreKey.js'
+import Header from '../../components/Header'
+import CurrentCity from '../../components/CurrentCity'
+import CityList from '../../components/CityList'
 
 class City extends React.Component {
     constructor(props, context) {
@@ -9,12 +18,37 @@ class City extends React.Component {
     render() {
         return (
             <div>
-                <h1>city</h1>
+                <Header title="选择城市"/>
+                <CurrentCity cityName={this.props.userinfo.cityName}/>
+                <CityList changeFn={this.changeCity.bind(this)}/>
             </div>
         )
     }
+    changeCity(newCity){
+        //修改redux
+        let userinfo = this.props.userinfo;
+        userinfo.cityName = newCity
+        this.props.userinfoActions.update(userinfo)
+
+        //修改localstroge
+        localStore.setItem(CITYNAME,newCity)
+
+        //跳转首页
+        hashHistory.push('/')
+    }
 }
 
-// 使用 require.ensure 异步加载，还不支持 ES6 的 export 
-// export default City
-module.exports = City
+function mapStateToProps(state) {
+    return {
+        userinfo : state.userinfo
+    }
+}
+function mapDispatchToProps(dispatch){
+    return {
+        userinfoActions : bindActionCreators(userinfoActionsFormOtherFile,dispatch)
+    }
+}
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps)
+(City)
