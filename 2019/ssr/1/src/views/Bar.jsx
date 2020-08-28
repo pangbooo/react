24dbca4e0ec23e5fb3a5d872e839ab84d2b77691
pangbo <1838529223@qq.com>
2020-08-28 20:57:21 +0800

    test:one page go back listener

--- 2019/ssr/1/src/views/Bar.jsx
+++ 2019/ssr/1/src/views/Bar.jsx
@@ -2,16 +2,42 @@ import React from 'react';
 import {Helmet} from 'react-helmet';
 
 class Bar extends React.Component {
-    render(){
-        return (
-            <div>
-              <Helmet>
-                <title>Bar</title>
-              </Helmet>
-              <div>Bar</div>
-            </div>
-          );
+  
+  componentDidMount(){
+    console.log('componentDidMount')
+  }
+
+  componentDidUpdate(prevProps, preState){
+    console.log('componentDidUpdate...........');
+    console.log(prevProps)
+    console.log(this.props);
+
+    if(prevProps.location.pathname !== this.props.location.pathname){
+      console.log('url has changed');
+      // this.forceUpdate();
+      // 使用forceUpdate(),会进入componentDidUpdate，不会进入componentDidMount
     }
+  }
+
+  handlePush(){
+    console.log('handlePush...............')
+    let { location } = this.props;
+    this.props.history.push(location.pathname + '/a')
+  }
+
+  render(){
+      return (
+          <div>
+            <Helmet>
+              <title>Bar</title>
+            </Helmet>
+            <div>
+              Bar
+              <button onClick={this.handlePush.bind(this)}>history push</button>
+            </div>
+          </div>
+        );
+  }
 }
 
 export default Bar;
