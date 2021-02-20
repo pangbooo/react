4b5253ee845979d9c851e7e99dcab718f080dba1
pangbo <1838529223@qq.com>
2021-02-20 14:42:20 +0800

    remove file

--- 2019/my-app-16/src/router.js
+++ 2019/my-app-16/src/router.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
-import About from './pages/About';
 import Home from './pages/Home';
-import Topics from './pages/Topics';
 import NoMatch from './pages/NoMatch';
-import Memoize from './pages/memoize';
-import RefForward from "./pages/RefForward";
 const list =[
   {
     id: 0,
@@ -21,18 +17,6 @@ function Menu() {
       <ul className='menu'>
         <li>
           <NavLink to="/" activeClassName="active" exact>Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about" activeClassName="active">About</NavLink>
-        </li>
-        <li>
-          <NavLink to="/topics" activeClassName="active">Topics</NavLink>
-        </li>
-        <li>
-          <NavLink to="/memoize" activeClassName="active">Memoize</NavLink>
-        </li>
-        <li>
-          <NavLink to="/refs" activeClassName="active">Ref转发</NavLink>
         </li>
       </ul>
     );
@@ -51,10 +35,6 @@ function AppRouter(){
                 <div className='content'>
                   <Switch>
                       <Route path='/' component={Home} exact></Route>
-                      <Route path='/about' component={About}></Route>
-                      <Route path='/topics' component={Topics}></Route>
-                      <Route path='/memoize' render={() => <Memoize list={list}/>} ></Route>
-                      <Route path='/refs' component={RefForward}></Route>
                       {/* when none of the above match, <NoMatch> will be rendered */}
                       <Route component={NoMatch}></Route>
                   </Switch>
