b3a0abf8256b9804b202ebe27ce4e6a4c7887f62
pangbo <pangbo@rakuten.dhc.com>
2021-03-25 16:08:55 +0800

    ssr3(4-1) router

---
+++ 2019/ssr/3/src/Routes.js
@@ -0,0 +1,7 @@
+import {Route} from 'react-router-dom';
+import React from 'react';
+import Home from './containers/Home';
+
+export default(
+    <div><Route path='/' component={Home}></Route></div>
+)
--- 2019/ssr/3/src/server/index.js
+++ 2019/ssr/3/src/server/index.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import React from 'react';
 import {renderToString} from 'react-dom/server';
-import Home from '../containers/Home';
+import { StaticRouter } from "react-router-dom";
+import Routes from '../Routes';
 
 const app = express();
 app.use(express.static('public'));
 const port = 3000;
-const content = renderToString(<Home />)
+
 
 app.get('/', (req, res) => {
+  const content = renderToString(
+    <StaticRouter  location={req.path} context={{}}>
+      {Routes}
+    </StaticRouter>
+  )
   res.send(`
   <html>
       <head>
