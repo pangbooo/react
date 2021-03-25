922ba2e91b5f299482265c35b1314d96257cabb2
pangbo <pangbo@rakuten.dhc.com>
2021-03-25 16:41:39 +0800

    ssr3(4-2) multi router

--- 2019/ssr/3/src/Routes.js
+++ 2019/ssr/3/src/Routes.js
@@ -1,7 +1,11 @@
 import {Route} from 'react-router-dom';
 import React from 'react';
 import Home from './containers/Home';
+import Login from './containers/Login';
 
 export default(
-    <div><Route path='/' component={Home}></Route></div>
+    <div>
+        <Route path='/' component={Home} exact></Route>
+        <Route path='/login' component={Login} exact></Route>
+    </div>
 )
--- 2019/ssr/3/src/server/index.js
+++ 2019/ssr/3/src/server/index.js
@@ -1,31 +1,13 @@
 import express from 'express';
-import React from 'react';
-import {renderToString} from 'react-dom/server';
-import { StaticRouter } from "react-router-dom";
-import Routes from '../Routes';
+import { render } from './util';
 
 const app = express();
 app.use(express.static('public'));
 const port = 3000;
 
 
-app.get('/', (req, res) => {
-  const content = renderToString(
-    <StaticRouter  location={req.path} context={{}}>
-      {Routes}
-    </StaticRouter>
-  )
-  res.send(`
-  <html>
-      <head>
-        <title>ssr</title>
-      </head>
-      <body>
-        <div id="root">${content}</div>
-        <script src='/index.js'></script>
-      </body>
-    </html>
-  `)
+app.get('*', (req, res) => {
+  res.send(render(req))
 })
 
 app.listen(port, () => {
