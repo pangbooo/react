468950576825d06701102fe62804c918ee21124a
pangbo <pangbo@rakuten.dhc.com>
2021-03-26 10:08:30 +0800

    ssr3_note

--- 2019/ssr/3/src/server/util.js
+++ 2019/ssr/3/src/server/util.js
@@ -5,6 +5,8 @@ import Routes from '../Routes';
 
 export const render = (req) => {
     const content = renderToString(
+        // 必传参数 location： 传入path，确认服务端渲染的路由
+        //          context：
         <StaticRouter  location={req.path} context={{}}>
           {Routes}
         </StaticRouter>
@@ -18,7 +20,8 @@ export const render = (req) => {
             </head>
             <body>
               <div id="root">${content}</div>
-              <script src='/index.js'></script>
+              <!--引入客户端打包js文件，执行事件绑定-->
+              <script src='/index.js'></script> 
             </body>
           </html>
         `
