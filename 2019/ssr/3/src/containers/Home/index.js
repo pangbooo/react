e3a665fa1397ca6d8feb155c722e366a89c459d8
pangbo <pangbo@rakuten.dhc.com>
2021-03-29 14:23:29 +0800

    ssr3(5-5) redux

--- 2019/ssr/3/src/containers/Home/index.js
+++ 2019/ssr/3/src/containers/Home/index.js
@@ -4,22 +4,50 @@ import Header from '../../components/Header.js';
 import {connect} from 'react-redux';
 import { getHomeList } from './store/actions';
 
-const Home = (props) => {
-  useEffect(() => {
-    props.getHomeList()
-  });
+// const Home = (props) => {
+//   useEffect(() => {
+//     props.getHomeList()
+//   });
 
-  return (
-      <div onClick={() => {console.log('click Home')}}>
-        <Header />
-        Hello, {props.name}
-          {
-            props.newsList.map(item => (
-              <div key={item.id}>{item.title}</div>
-            ))
-          }
-      </div>
-  )
+//   return (
+//       <div onClick={() => {console.log('click Home')}}>
+//         <Header />
+//         Hello, {props.name}
+//           {
+//             props.newsList.map(item => (
+//               <div key={item.id}>{item.title}</div>
+//             ))
+//           }
+//       </div>
+//   )
+// }
+
+class Home extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  componentDidMount(){
+    this.props.getHomeList()
+  }
+
+  getHomeList(){
+    return(
+      this.props.newsList.map(item => (
+        <div key={item.id}>{item.title}</div>
+      ))
+    )
+  }
+  
+  render(){
+    return (
+            <div onClick={() => {console.log('click Home')}}>
+              <Header />
+              Hello, {this.props.name}
+               {this.getHomeList()}
+            </div>
+        )
+  }
 }
 
 const mapStateToProps  = (state) => ({
