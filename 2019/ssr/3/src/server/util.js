d07651d07112f4f264b422e8c115fb3785e0eb48
pangbo <pangbo@rakuten.dhc.com>
2021-03-30 16:52:05 +0800

    ssr3(5-9) 1）favicon。ico 2）多级路由

--- 2019/ssr/3/src/server/util.js
+++ 2019/ssr/3/src/server/util.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {renderToString} from 'react-dom/server';
-import { StaticRouter, Route, matchPath  } from "react-router-dom";
+import { StaticRouter, Route  } from "react-router-dom";
+import { matchRoutes } from "react-router-config"; //react-router-config 可以匹配多级路由
 import {Provider} from 'react-redux'
 import Routes from '../Routes';
 import getStore from '../store'
@@ -11,16 +12,10 @@ export const render = (req) => {
     //此处获取异步数据，存到store中
     //根据路由，往store添加数据
 
-    const matchRoutes = [];
-    Routes.some( route => {
-      const match = matchPath(req.path, route);
-      if(match) {
-        matchRoutes.push(route)
-      }
-    });
+    const matchedRoutes = matchRoutes(Routes, req.path)
 
     //让matchRoutes匹配的路由组件，执行组件的loadData
-    console.log('matchRoutes', matchRoutes)
+    console.log('matchedRoutes', matchedRoutes)
     
     const content = renderToString(
         // 必传参数 location： 传入path，确认服务端渲染的路由
